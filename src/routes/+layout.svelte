5208ec1ee5d2059a0e595340ea2a918840a70cca
Simar <simar@Simars-MacBook-Air.local>
2025-04-13 00:42:42 +0200

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import "../app.css";
+  import Searchbar from "../components/Navbar.svelte";
+  let { children } = $props();
+</script>
+
+<Searchbar />
+{@render children()}
---
+++ svelte.config.js
@@ -0,0 +1,11 @@
+import { mdsvex } from "mdsvex";
+import adapter from "@sveltejs/adapter-auto";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+
+const config = {
+  preprocess: [vitePreprocess(), mdsvex()],
+  kit: { adapter: adapter(), alias: { $assets: "static" } },
+  extensions: [".svelte", ".svx"],
+};
+
+export default config;

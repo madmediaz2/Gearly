56914cba404c2247e9962c39c597af20f32297b8
madmediaz2 <simar.cakmak@gmail.com>
2025-04-17 11:00:00 +0200

    checkpoint

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import ShopItem from "../components/ShopItem.svelte";
+    function test(){
+
+    }
+</script>
+
+
+<ShopItem name="Playstation 5" image="" brand="Sony" price={10} onAddToCart={test}/>
